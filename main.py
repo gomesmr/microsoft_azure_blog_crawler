0b605527027c8d7511964c1d21a7a4995fc9b434
Marcelo Gomes <marcelo.gomes@zup.com.br>
2023-04-04 22:54:43 -0300

    Refatoração 01

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from azure_blog_crawler import AzureBlogCrawler
 
 
-def main():
-    begin_process_on_page = 1
-    batches = 1
-    pages_per_batch = 1
+if __name__ == "__main__":
+    first_page = 1
+    batches = 2
+    pages_per_batch = 3
+    delay_between_batches = 2
 
-    crawler = AzureBlogCrawler(begin_process_on_page, batches, pages_per_batch)
-    crawler.crawl_multiple_batches()
-
-
-if __name__ == '__main__':
-    main()
+    with AzureBlogCrawler(first_page, batches, pages_per_batch, delay_between_batches) as crawler:
+        crawler.crawl_multiple_batches()
